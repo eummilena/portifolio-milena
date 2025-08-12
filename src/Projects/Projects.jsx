c833b46d0088338e8cd1055d1ec8b0a371792901
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-08-12 15:08:09 -0300

    feat - componente criado

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './Projects.module.css'
+import bolinhas from '../Assets/bolinhas.svg'
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <main className={styles.main}>
+            <h1><span className='barra'>/</span>projetos<span className='dot'>.</span></h1>
+            <section className={styles.section}>
+                <div className={styles.square}></div>
+                <div className={styles.square}></div>
+                <div className={styles.square}></div>
+            </section>
+            <img src={bolinhas} className={styles.bolinhas} alt="" />
+        </main >
     )
 }
 
