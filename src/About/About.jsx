5dabc23de9e8465265b6439eb6bbb552c149fe28
Milena Mota <80923819+eummilena@users.noreply.github.com>
2026-01-06 17:06:25 -0300

    docs: fotos add ao README e alteração no cv

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -10,11 +10,11 @@ const About = () => {
             <Head title='Sobre' />
             <h1><span className='barra' aria-hidden="true">/</span>sobre <span className='dot' aria-hidden="true">.</span></h1>
             <div className={styles.text} aria-label='Texto descritivo sobre mim' >
-                <p>Olá, eu sou a <strong>Milena</strong>! Trabalho como desenvolvedora front-end há 2 anos, criando e dando vida a sites e aplicativos. Gosto de transformar ideias em projetos funcionais e visualmente agradáveis, unindo código limpo a boas práticas de UI/UX. Atualmente, estou mergulhada no universo do React e TypeScript, sempre buscando aprender mais e criar experiências que sejam práticas e agradáveis para o usuário.</p>
+                <p>Olá, eu sou a <strong>Milena</strong>! Trabalho como desenvolvedora front-end há mais de 2 anos, criando e dando vida a sites e aplicativos. Gosto de transformar ideias em projetos funcionais e visualmente agradáveis, unindo código limpo a boas práticas de UI/UX. Atualmente, estou mergulhada no universo do React e TypeScript, sempre buscando aprender mais e criar experiências que sejam práticas e agradáveis para o usuário.</p>
                 <img src={Poligonos} className={styles.poligonos} alt="" aria-hidden="true" />
             </div>
             <div className={styles.buttonContainer}>
-                <Button ariaLabel='Baixar currículo em PDF' href="/curriculo.pdf" download="curriculo-milena-frontend.pdf">Baixar cv</Button>
+                <Button ariaLabel='Baixar currículo em PDF' href="/curriculo.pdf" download="Milena_Mota_Desenvolvedora_Frontend.pdf">Baixar cv</Button>
                 <img src={bolinhas} className={styles.bolinhas} alt="" aria-hidden="true" />
             </div>
         </main>
